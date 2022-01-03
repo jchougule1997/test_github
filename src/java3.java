d16102fa0215859e7fae374c723ae388a141657e
KARAN <KARAN@DESKTOP-NKEBQD8>
2022-01-03 10:41:11 +0530

    java

---
+++ src/java3.java
@@ -0,0 +1,13 @@
+
+public class java3 {
+
+	public static void main(String[] args) {
+
+
+
+		System.out.println("java");
+		
+		
+	}
+
+}
--- src/verify_test2.java
+++ src/verify_test2.java
@@ -2,7 +2,8 @@
 public class verify_test2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("hey jack");
 
 	}
 
